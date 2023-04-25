57db9650c8eb0251c562e79d7d5e65391d9a6cb3
FoggieB <131722600+FoggieB@users.noreply.github.com>
2023-04-24 21:45:08 -0400

    tuning time lol
    updated siminit and simulink model

--- sim_init.m
+++ sim_init.m
@@ -9,4 +9,45 @@ zetas = 0.7;
 wt = 1;
 zetat = 0.7;
 
+%sim setup
+Iw = 3;
+Ic = 30;
+m = 80;
+r = 3;
+R = 15;
+wheel_damp = 1;
+turn_damp = 1;
+
+wdot0 = -2; %have to make a constant to get reference model
+psi0 = 2;  %have to make a constant to get reference model
+
+%base model
+
+Ap = [0 1 0; 0 -wheel_damp 0; 0 0 -turn_damp];
+Bp = [0 0; r*Iw/(m*r) 0; Iw*R*psi0/Ic Iw*R*wdot0/Ic];
+Cp =  [0 1 0;0 0 1]; 
+Dp = [1 0; 0 1];
+lambda = [1 0; 0 1];
+
+%extended model
+
+A = [zeros(2,2) Cp;...
+     zeros(3,2) Ap];
+B = [Dp;...
+     Bp];
+C = [zeros(2,2) Cp];
+D=Dp;
+Q = diag([1 1 1 1 1]);
+R = diag([1 1]);
+Kxt = lqr(A,B,Q,R);
+Aref = A-B*Kxt;
+Cref = C;
+Bref = [-eye(2);zeros(3,2)];
+Gamau = 500;
+Qref = diag([1 1 1 1 1]);
+Pref = lyap(Aref',Qref);
+
+run('carSim')
+close all;
+
 
